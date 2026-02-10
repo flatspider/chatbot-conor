4c843d9af5c161df958e01c32028fddf73b122da
Conor McManamon <c.s.mcmanamon@gmail.com>
2026-02-10 13:19:30 -0500

    Added dotenv. Passing in env properly

--- chatbot-conor/server.js
+++ chatbot-conor/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import dotenv from "dotenv";
 
 const PORT = 3000;
 
@@ -9,7 +10,10 @@ app.use(express.json());
 // This backend should be fetching to anthropic and pushing data to frontend
 
 app.get("/message", (req, res) => {
-  res.json({ message: "hello from backend" });
+  res.json({
+    message: "hello from backend",
+    secret: process.env.ANTHROPIC_SECRET_KEY,
+  });
 });
 
 ViteExpress.listen(app, PORT, () => {
