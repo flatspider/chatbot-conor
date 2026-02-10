abca382816ac20e8968f85ba625242bcf3257b56
Conor McManamon <c.s.mcmanamon@gmail.com>
2026-02-10 14:22:57 -0500

    Functioning Anthropic api call initiated from frontend

--- chatbot-conor/server.js
+++ chatbot-conor/server.js
@@ -16,6 +16,33 @@ app.get("/message", (req, res) => {
   });
 });
 
+app.post("/hello", async (req, res) => {
+  //Take in the message from the request
+  const message = req.body.message;
+  //Build out the fetch to anthropics endpoint
+  console.log(message);
+  let response = await fetch("https://api.anthropic.com/v1/messages", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "anthropic-version": "2023-06-01",
+      "x-api-key": process.env.ANTHROPIC_SECRET_KEY,
+    },
+    body: JSON.stringify({
+      model: "claude-haiku-4-5-20251001",
+      max_tokens: 1000,
+      messages: [
+        {
+          role: "user",
+          content: message,
+        },
+      ],
+    }),
+  });
+  let anthropic_response = await response.json();
+  res.json(anthropic_response);
+});
+
 ViteExpress.listen(app, PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
--- chatbot-conor/src/App.tsx
+++ chatbot-conor/src/App.tsx
@@ -2,10 +2,41 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState<string>("");
+  const [conversation, setConversation] = useState<string>("");
+
+  const handleClick = async () => {
+    // Hit the /hello endpoint with message
+    // Also feels like the compounded chat should live on the server
+    // And lets put in a time management system
+    // Also want the tokenizer lab
+    console.log(message);
+    const response = await fetch("/hello", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message }),
+    });
+    const json = await response.json();
+    setConversation(json);
+  };
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card"> </div>
+      <div>
+        <h1>ChatBot</h1>
+        <input
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        ></input>
+        <button onClick={handleClick} className="send-button">
+          Send!
+        </button>
+      </div>
+      <textarea>{conversation}</textarea>
     </>
   );
 }
