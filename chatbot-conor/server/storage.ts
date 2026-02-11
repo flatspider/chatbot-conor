5ecff83defdabc44e53fb41e9293988b098d6b47
Conor McManamon <c.s.mcmanamon@gmail.com>
2026-02-11 13:45:05 -0500

    Initial class InMemoryStorage implementation

--- chatbot-conor/server/storage.ts
+++ chatbot-conor/server/storage.ts
@@ -1,3 +1,52 @@
 // File defining interface
 
+import { UUID , randomUUID } from "node:crypto";
 
+
+//Define types
+
+export type Message = {
+  content: string,
+  role: 'user' | 'assistant',
+}
+
+export type Conversation = {
+    messages: Message[];
+    conversationID: UUID;
+}
+
+export interface Storage {
+    createConversation: () => Conversation;
+    getConversation: (conversationID: UUID) => Conversation | null;
+    getConversations: () => Conversation[];
+    addMessageToConversations: (message: Message, conversationID: UUID) => void;
+}
+
+export class InMemoryStorage implements Storage {
+    // Creates the private array of convos that these methods can reach into
+    private conversations: Conversation[] = []
+    createConversation(): Conversation { 
+        let convoID = randomUUID();
+        let newConversation: Conversation = {
+            messages: [],
+            conversationID: convoID
+        }
+        return newConversation;
+    }
+    getConversation(convoID: UUID): Conversation | null { 
+        let targetConvo = this.conversations.find(convo => convo.conversationID === convoID)
+        if(targetConvo) {
+            return targetConvo;
+        } else {
+            return null;
+        }
+     }
+    getConversations(): Conversation[] {
+        return this.conversations;
+    }
+    addMessageToConversations(message: Message, convoID: UUID): void {
+        let targetConversation = this.getConversation(convoID);
+        // Looking at conversation. Add to conversation.messages;
+        targetConversation?.messages.push(message);
+    }
+}
