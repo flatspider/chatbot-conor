7ece1b0e2e1a7bfa0b42893b74167282dd5f6409
Conor McManamon <c.s.mcmanamon@gmail.com>
2026-02-11 14:56:11 -0500

    Two passing tests

--- chatbot-conor/server/storage.test.ts
+++ chatbot-conor/server/storage.test.ts
@@ -1,5 +1,5 @@
 import {expect, test} from 'vitest';
-import {InMemoryStorage} from './storage';
+import {InMemoryStorage, type Message} from './storage';
 
 // Test if create conversation adds to the array
 
@@ -8,6 +8,11 @@ let testData = new InMemoryStorage;
 test('create a conversation', ()=>{
     testData.createConversation();
     expect(testData.getConversations().length).toBeGreaterThanOrEqual(1);
+
+    testData.createConversation();
+    let convoArray = testData.getConversations();
+
+    expect(convoArray[0].conversationID !== convoArray[1].conversationID);
 });
 
 test('add a message', ()=> {
@@ -17,10 +22,11 @@ test('add a message', ()=> {
 
     // Need a message and convo ID.
     // Have to specify that user is not just an arbitrary string. 
-    // You have to lock it in for 
-    let testMessage = {
+    // You have to lock it in for TypeScript. Either role: "user" as const,
+    // Or setting the message as a const. 
+    const testMessage: Message = {
         content: "Hello my first message",
-        role: "user" as const,
+        role: "user",
     }
     testData.addMessageToConversations(testMessage, testConvoID);
 
