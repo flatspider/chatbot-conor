7de838e350230666863556477d5171cac6a7c1cf
Conor McManamon <c.s.mcmanamon@gmail.com>
2026-02-12 15:50:53 -0500

    Basic routes scaffolding setup

---
+++ chatbot-conor/src/components/ChatPage.tsx
@@ -0,0 +1,5 @@
+export const ChatPage = () => {
+  return <div>THIS IS CHAT PAGE</div>;
+};
+
+export default ChatPage;
--- chatbot-conor/src/main.tsx
+++ chatbot-conor/src/main.tsx
@@ -3,12 +3,19 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
+import { ChatPage } from "./components/ChatPage.tsx";
+import { LoginPage } from "./components/LoginPage.tsx";
+import { NewChatPage } from "./components/NewChatPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route element={<App />}>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/new" element={<NewChatPage />} />
+          <Route path="/chat/:chatID" element={<ChatPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
